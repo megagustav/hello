f73e1032c0ff4f38f9756773a217a19fd7a821e8
Gustav Moorhouse <g.moorhouse@me.com>
2020-06-15 20:04:03 +0200

    added padding to i, svg

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -28,19 +28,19 @@
  
  	<div class="row my-4">
 		<div class="col">
-			<a class="mb-2 mb-sm-0 col-auto btn btn-primary btn-block category-kunst" href="category/kunst" role="button"><i class="fas fa-pencil-paintbrush"></i>&nbsp;Kunst</a>
+			<a class="mb-2 mb-sm-0 col-auto btn btn-primary btn-block category-kunst" href="category/kunst" role="button"><i class="fas fa-pencil-paintbrush"></i>Kunst</a>
 		</div>
 		<div class="col">
-			<a class="col-auto btn btn-primary btn-block category-klima" href="category/klima" role="button"><i class="fas fa-leaf"></i>&nbsp;Klima</a>
+			<a class="col-auto btn btn-primary btn-block category-klima" href="category/klima" role="button"><i class="fas fa-leaf"></i>Klima</a>
 		</div>
 		<div class="col">
-			<a class="col-auto btn btn-primary btn-block category-jugend" href="category/jugend" role="button"><i class="fas fa-grin-alt"></i>&nbsp;Jugend</a>
+			<a class="col-auto btn btn-primary btn-block category-jugend" href="category/jugend" role="button"><i class="fas fa-grin-alt"></i>Jugend</a>
 		</div>
 		<div class="col">
-			<a class="col-auto btn btn-primary btn-block category-altenpflege" href="category/senioren" role="button"><i class="fas fa-heart"></i>&nbsp;Senioren</a>
+			<a class="col-auto btn btn-primary btn-block category-altenpflege" href="category/senioren" role="button"><i class="fas fa-heart"></i>Senioren</a>
 		</div>
 		<div class="col">
-			<a class="col-auto btn btn-primary btn-block category-sport" href="category/sport" role="button"><i class="fas fa-tennis-ball"></i>&nbsp;Sport</a>
+			<a class="col-auto btn btn-primary btn-block category-sport" href="category/sport" role="button"><i class="fas fa-tennis-ball"></i>Sport</a>
 		</div>
  	</div>
  
