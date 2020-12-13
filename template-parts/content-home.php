baabc4f68037ca1521c4d6188ae95552f2fccaf0
Gustav Moorhouse <g.moorhouse@me.com>
2020-12-13 16:58:03 +0100

    Update content-home.php

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -60,7 +60,7 @@
 	<p>Wollen Sie über neue Projekte informiert bleiben? Haben Sie bereits ein Projekt unterstützt und möchten erfahren, sobald wir Neuigkeiten haben? Melden Sie sich für unseren kostenlosen Newsletter an.</p>
 	
 	<!-- START - We recommend to place the below code where you want the form in your website html  -->
-	<div class="sib-form">
+	<div class="">
 	  <div id="sib-form-container">
 		<div id="error-message" class="sib-form-message-panel" style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;max-width:540px;">
 		  <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
@@ -86,11 +86,11 @@
 		  </div>
 		</div>
 		<div></div>
-		<div id="sib-container" class="sib-container--large sib-container--vertical">
+		<div id="sib-container" class="">
 		  <form id="sib-form" method="POST" action="https://45aab95b.sibforms.com/serve/MUIEANlCwQ8rBeWUMt7xCPy4apsMBXBypuBHBf0IS6YjQKxmyHJMhb_Hgj4v7pIgI1oNlnQ6WpbFdv_5lbxgzSmcc2xnI40yJyARVOfPxkvwVHY-voIGmMhb8zJIn-JUVSGWM2mmE8mcV79z246m9lGRbkWfr_X0ZVh4SQvNdz-PU17UCEGTl7w78pZgJCR_wCYkE-jyAzHtasaa"
 			data-type="subscription">
-			<div style="padding: 8px 0;">
-			  <div class="sib-input sib-form-block">
+			<div style="">
+			  <div class="">
 				<div class="form__entry entry_block">
 				  <div class="form__label-row form-inline mt-5">
 					  
@@ -105,13 +105,7 @@
 						  </div>
 						</div>
 						
-						<button class="col-sm-2 px-0 btn btn-block btn-primary mb-2 sib-form-block__button sib-form-block__button-with-loader" form="sib-form" type="submit">
-							  <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512">
-								<path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"
-								/>
-							  </svg>
-							  Abonnieren
-							</button>
+						<button class="col-sm-2 px-0 btn btn-block btn-primary mb-2" form="sib-form" type="submit">Abonnieren</button>
 
 					
 				  </div>
